390162dec775adb475ddd8cb9f082246d02057ec
Gusthavo Queiroz <160295726+gusthavoqueiroz@users.noreply.github.com>
2024-10-19 16:55:48 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de programação de implementação do https://.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Eu amo você Jenyfer! Se você também me ama, da uma risadinha </h1>
+  return <h1>Eu amo você Jenyfer! Se você também me ama, da uma risadinha </h1>;
+}
+
+function teste2() {
+  console.log("teste_indentacao");
 }
 
 export default Home;
